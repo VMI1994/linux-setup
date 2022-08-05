3986307a183830193f0670e0615e9d05e710424d
Jason E <ensminger@yahoo.com>
2022-08-05 11:33:07 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+
+# update the system and install tmux
+echo "updating system and installing nala and tmux"
+sudo update
+sudo apt install tmux nala
+echo "starting upgrade"
+sudo apt upgrade -y
+
+# Install common programs
+
+
+
+# install aliases for terminal
+echo 'alias ll="ls -lha"' >> ~/.bashrc
+echo 'alias bashrc="nano ~/.bashrc && exec bash"' >> ~/.bashrc
+echo 'alias help="cat ~/.bashrc | grep alias"' >> ~/.bashrc
+exec bash
+exit
