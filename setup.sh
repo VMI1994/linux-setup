b987d9f20b5dae10aeafc878284458812c239719
Jason E <ensminger@yahoo.com>
2022-08-05 11:40:38 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,19 +4,31 @@ clear
 
 # update the system and install tmux
 cd ~/
-echo "updating system and installing nala and tmux"
-sudo update
-sudo apt install tmux nala
-echo "starting upgrade"
-sudo apt upgrade -y
+echo "Updating System"
+sleep 1
+sudo apt update
+echo "Starting Upgrade"
+sudo apt upgrade
+clear
+
 
 # Install common programs
-
+clear
+echo "Installing common programs using nala..."
+sleep 1
+sudo apt install nala
+sudo nala install neofetch tmux 
 
 
 # install aliases for terminal
+clear
+echo "Installing common aliases"
+sleep 1
 echo 'alias ll="ls -lha"' >> ~/.bashrc
 echo 'alias bashrc="nano ~/.bashrc && exec bash"' >> ~/.bashrc
 echo 'alias help="cat ~/.bashrc | grep alias"' >> ~/.bashrc
 exec bash
+
+clear
+echo "Setup is Complete"
 exit
