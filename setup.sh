3661d330e040960d0566fb9184158de06eb389e9
Jason E <ensminger@yahoo.com>
2022-08-06 11:46:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,15 +59,45 @@ fi
 # Cleaning up unneeded files
 sudo apt autoremove
 
-# Anything below this line has yet to be scripted
-# Any recommendations for bash aliases and good programs to add are welcome
 
-# Generate SSH keys
+# Set up unattended-upgrades (Credit to Jay LaCroix - learnlinux.tv)
+clear
+echo "Setting up unattended-upgrades...at the prompt please select 'Yes'..."
+sleep 5
+sudo dpkg-reconfigure --priority=low unattended-upgrades
+sudo mv /etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades.old
+sudo cp ~/linux-setup/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+sudo mv /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades.old
+sudo cp ~/linux-setup/50unattended-upgrades /etc/aptapt.conf.d/50unattended-upgrades
+sleep 2
 
-# Lock down SSH
+# Configure OpenSSH-Server (Credit to Jay LaCroix - learnlinux.tv)
+clear
+echo "Configuring OpenSSH-Server"
+sleep 1
+echo "Regenerating SSH keys....Please press enter at all prompts..."
+sudo ssh-keygen -t ed25519
+sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old
+sudo cp ~/linux-setup/sshd_config /etc/ssh/sshd_config
+echo "SSH configured....restarting service"
+sleep 2
+sudo systemctl restart sshd
+echo "SSH service restarted"
+sleep 2
 
-# Fix anything related to cloud-init
 
-# Any other security related concerns
+# Setup Fail2ban
+clear
+echo "Setting up Fail2ban"
+sudo cp ~/linux-setup/jail.local /etc/fail2ban/jail.local
+echo "Fail2ban is Setup"
+sleep 2
 
-
+# Cleanup & exit
+clear
+echo "setup is complete...."
+sleep 1
+echo "Setup is now erasing files and exiting"
+sleep 1
+sleep 5 && sudo rm -rf ~/linux-setup
+exit
