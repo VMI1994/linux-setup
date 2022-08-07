b78851942397d4e3d10f1e22e3abd9cd48175fcd
Jason E <ensminger@yahoo.com>
2022-08-07 11:43:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ echo 'Installing and activating aliases'
 sleep 1
 cat ~/linux-setup/alias.txt >> ~/.bashrc
 bash ~/linux-setup/alias.sh &
-echo 'done...'
-sleep 1
+echo 'done...Press Enter to continue'
+read junk
 
 
 # Install respositories
@@ -18,7 +18,8 @@ echo "Installing additional repositories"
 sudo add-apt-repository -y ppa:teejee2008/timeshift #repo for timeshift
 echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list #nala repo
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null #nala key
-sleep 2
+echo "Done, press Enter to continue"
+read junk
 
 
 # Update system
@@ -26,7 +27,8 @@ clear
 echo "Updating the system"
 sudo apt update
 sudo apt dist-upgrade -y
-
+echo "The system has been updated, Press Enter to contue"
+read junk
 
 # Remove cloud-init if present and Install common programs
 clear
@@ -39,8 +41,9 @@ then
 else
   echo "Cloud-init not installed, continuing"
 fi
+sleep 2
+clear
 echo "Installing common programs"
-sleep 1
 # Create list of apps to install from apps.txt
 apps=(`cat ~/linux-setup/apps.txt`)
 noofelements=${#apps[*]}
@@ -53,30 +56,35 @@ do
     counter=$(( $counter + 1 ))
 done
 apps=("$list")
-sudo apt install $apps
+sudo apt install $apps -y
 curl -s https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
-
+echo "Programs installed, Press Enter to continue"
+read junk
 
 # Cleaning up unneeded files
+echo "Removing unused packages"
 sudo apt autoremove
-
+echo "Done, Press Enter"
+read junk
 
 # Set up unattended-upgrades (Credit to Jay LaCroix - learnlinux.tv)
 clear
 echo "Setting up unattended-upgrades...at the prompt please select 'Yes'..."
-sleep 5
+echo "Press Enter to continue"
 sudo dpkg-reconfigure --priority=low unattended-upgrades
 sudo rm /etc/apt/apt.conf.d/20auto-upgrades
 sudo cp ~/linux-setup/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
 sudo rm /etc/apt/apt.conf.d/50unattended-upgrades
 sudo cp ~/linux-setup/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
-sleep 2
+echo "Press Enter to continue"
 
 # Configure OpenSSH-Server (Credit to Jay LaCroix - learnlinux.tv)
 clear
 echo "Configuring OpenSSH-Server"
 sleep 1
 echo "Regenerating SSH keys....Please press enter at all prompts..."
+echo "Press Enter to continue"
+read junk
 sudo ssh-keygen -t ed25519
 sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old
 sudo cp ~/linux-setup/sshd_config /etc/ssh/sshd_config
@@ -84,7 +92,6 @@ echo "SSH configured....restarting service"
 sleep 2
 sudo systemctl restart sshd
 echo "SSH service restarted"
-sleep 2
 
 
 # Setup Fail2ban
@@ -92,10 +99,12 @@ clear
 echo "Setting up Fail2ban"
 sudo cp ~/linux-setup/jail.local /etc/fail2ban/jail.local
 echo "Fail2ban is Setup"
-sleep 2
+echo "Press Enter"
+read junk
 
 # Attempt to install nala (frontend for apt package manager)
-echo "Installing nala"
+echo "Installing nala, press Enter to continue"
+read junk
 sudo apt install nala
 
 # Cleanup & exit
